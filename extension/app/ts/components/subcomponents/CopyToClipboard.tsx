225c173db5adda13fa297c53acec7ff04722f3f8
Killari <Killari@gmail.com>
2022-12-27 11:29:35 +0200

    remove preact.VNode

--- extension/app/ts/components/subcomponents/CopyToClipboard.tsx
+++ extension/app/ts/components/subcomponents/CopyToClipboard.tsx
@@ -1,13 +1,14 @@
+import { ComponentChildren } from 'preact'
 import { clipboardCopy } from './clipboardcopy.js'
 
 interface Props {
-	children: preact.VNode
+	children: ComponentChildren
 	content: string
 	contentDisplayOverride?: string
 	copyMessage: string | undefined
 }
 
-export function CopyToClipboard(props: Props): preact.VNode {
+export function CopyToClipboard(props: Props) {
 	return (
 		<div onClick = { () => { clipboardCopy(props.content) } } style = 'display: inherit; overflow: inherit;'>
 			<div
