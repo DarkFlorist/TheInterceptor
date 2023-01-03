dc72c646fae430d6f93d559975f1302eb11c41a9
Killari <Killari@gmail.com>
2023-01-03 09:20:30 +0200

    skeleton of address book

--- extension/app/ts/components/pages/AddressBook.tsx
+++ extension/app/ts/components/pages/AddressBook.tsx
@@ -1,131 +1,44 @@
-import { useState, useEffect } from 'preact/hooks'
-import { getChainName, isSupportedChain } from '../../utils/constants.js'
-import { Error, ErrorCheckBox } from '../subcomponents/Error.js'
-
-interface InterceptorChainChangeRequest {
-	isInterceptorSupport: boolean,
-	chainName: string,
-	origin: string,
-	icon: string | undefined,
-	simulationMode: boolean,
-}
+import { useEffect } from 'preact/hooks'
 
 export function AddressBook() {
-	const [chainChangeData, setChainChangeData] = useState<InterceptorChainChangeRequest | undefined>(undefined)
-	const [connectAnyway, setConnectAnyway] = useState<boolean>(false)
 
 	useEffect( () => {
 		function popupMessageListener(msg: unknown) {
 			console.log('popup message')
 			console.log(msg)
-			fetch()
 		}
 		browser.runtime.onMessage.addListener(popupMessageListener)
-
-		fetch()
 
 		return () => {
 			browser.runtime.onMessage.removeListener(popupMessageListener)
 		};
 	}, []);
 
-	async function fetch() {
-		const backgroundPage = await browser.runtime.getBackgroundPage()
-		if( !('changeChainDialog' in backgroundPage.interceptor) || backgroundPage.interceptor.changeChainDialog === undefined) return window.close();
-		const dialog = backgroundPage.interceptor.changeChainDialog
-		setChainChangeData( {
-			isInterceptorSupport : isSupportedChain(dialog.chainId),
-			chainName : getChainName(BigInt(dialog.chainId)),
-			origin: dialog.origin,
-			icon: dialog.icon,
-			simulationMode: dialog.simulationMode,
-		} )
-	}
-
-	function approve() {
-		browser.runtime.sendMessage( { method: 'popup_changeChainDialog', options: { accept: true } } )
-	}
-
-	function reject() {
-		browser.runtime.sendMessage( { method: 'popup_changeChainDialog', options: { accept: false } } )
-	}
-
 	return (
 		<main>
-		{ chainChangeData === undefined ? <></> : <>
-			<div className = 'block' style = 'margin-bottom: 0px; margin: 10px'>
-				<header class = 'card-header window-header'>
-					<div class = 'card-header-icon unset-cursor'>
-						<span class = 'icon'>
-							<img src = '../img/access-key.svg'/>
-						</span>
-					</div>
-					<p class = 'card-header-title'>
-						<p className = 'paragraph'>
-							Chain Change Request
-						</p>
-					</p>
-				</header>
-				<div class = 'card-content'>
-					<article class = 'media'>
-						{
-							chainChangeData.icon === undefined ? <></> :
-								<figure class = 'media-left' style = 'margin: auto; display: block; padding: 20px'>
-									<p class = 'image is-64x64'>
-										<img src = { chainChangeData.icon }/>
-									</p>
-								</figure>
-						}
-					</article>
-					<div class = 'media-content' style = 'padding-bottom: 10px'>
-						<div class = 'content'>
-							<p className = 'title' style = 'white-space: normal; text-align: center;'>
-								<p className = 'title' style = 'white-space: normal; text-align: center; font-weight: bold;'>
-									{ chainChangeData.origin }
-								</p>
-								would like to switch to
-								<p className = 'title' style = 'white-space: normal; text-align: center; font-weight: bold;'>
-									{ chainChangeData.chainName }
-								</p>
-								{ !chainChangeData.isInterceptorSupport && chainChangeData.simulationMode ?
-									<div style = 'font-size: 0.5em;'>
-										<Error
-											text = { 'This chain is not supported by The Interceptor. If you want to use this chain anyway. Select Signing mode instead of Simulation mode and attempt to change the chain again. You will then be able to disable The Interceptor and send transactions without protection of The Interceptor.' }
-										/>
-									</div>
-								: <></> }
-								{ !chainChangeData.isInterceptorSupport && !chainChangeData.simulationMode ?
-									<div style = 'font-size: 1em;'>
-										<ErrorCheckBox
-											text = { 'This chain is not supported by The Interceptor. Would you like to disable The Interceptor and attempt to connect anyway?' }
-											checked = { connectAnyway }
-											onInput = { setConnectAnyway }
-										/>
-									</div>
-								: <></> }
-							</p>
-						</div>
-					</div>
-					<div style = 'overflow: auto; display: flex; justify-content: space-around; width: 100%; height: 40px;'>
-						<button
-							className = { `button is-primary ${ !chainChangeData.isInterceptorSupport ? 'is-danger' : '' }` }
-							disabled = { !chainChangeData.isInterceptorSupport && ( (!connectAnyway && !chainChangeData.simulationMode ) || chainChangeData.simulationMode ) }
-							style = 'flex-grow: 1; margin-left: 5px; margin-right: 5px;'
-							onClick = { approve }>
-							{ chainChangeData.isInterceptorSupport ? 'Change chain' : 'Disable The Interceptor and change' }
-						</button>
-						<button
-							className = { `button is-primary ${ chainChangeData.isInterceptorSupport ? 'is-danger' : '' }` }
-							style = { `flex-grow: 1; margin-left: 5px; margin-right: 5px;` }
-							onClick = { reject } >
-							Don't change
-						</button>
-					</div>
+			<div class="columns" style = 'margin: 10px'>
+				<div class="column is-2">
+					<aside class="menu">
+						<ul class="menu-list">
+							Active Addresses
+						</ul>
+						<ul class="menu-list">
+							Contacts
+						</ul>
+						<ul class="menu-list">
+							Contracts
+						</ul>
+						<ul>
+							<li><a>Tokens</a></li>
+							<li><a>Non Fungible Tokens</a></li>
+							<li><a>Other</a></li>
+						</ul>
+					</aside>
+				</div>
+				<div class="column">
+					<p class="bd-notification is-primary">Second column with more content. This is so you can see the vertical alignment.</p>
 				</div>
 			</div>
-
-			<div class = 'content' style = 'height: 0.1px'/>
-		</> }
 		</main>
 	)
 }
