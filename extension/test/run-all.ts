af6ab83ddc15e80ee3ee9b257929c56e7f0384e0
Killari <Killari@gmail.com>
2023-01-16 17:13:56 +0200

    refactor using refactored micro-should

--- extension/test/run-all.ts
+++ extension/test/run-all.ts
@@ -1,19 +1,29 @@
-import { GREEN, RED, RESET, runIfRoot } from './runTestCases.js'
-import { main as testUtilsBigint } from './utils.bigint.js'
-import { main as testMetadataUtils } from './metadataUtils.js'
+import { runIfRoot, run } from './micro-should.js'
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
 
-async function main() {
-	const result = [
-		await testUtilsBigint(),
-		await testMetadataUtils(),
-	].every(item => item === true)
-
-	if (!result) {
-		console.error(`${ RED }TESTS FAILED!${ RESET }`)
-		process.exit(1);
+function *walkSync(dir: string) : Generator<string> {
+	const files = fs.readdirSync(dir, { withFileTypes: true });
+	for (const file of files) {
+		if (file.isDirectory()) {
+			yield* walkSync(path.join(dir, file.name));
+		} else {
+			yield path.join(dir, file.name);
+		}
 	}
-	console.log(`${ GREEN }TESTS SUCCEEDED!${ RESET }`)
-	process.exit(0);
 }
 
-await runIfRoot(main, import.meta)
+const __filename = fileURLToPath(import.meta.url);
+const TEST_DIRECTORY = path.join(path.dirname(__filename), 'tests')
+
+await runIfRoot(async () => {
+	for (const file of walkSync(TEST_DIRECTORY)) {
+		const relativeFilePath = `./${ path.relative(path.dirname(__filename), file) }`
+		const imported = await import(relativeFilePath)
+		if( ! ('main' in imported) && typeof imported.main === 'function') throw new Error(`missing main function in ${ relativeFilePath }`)
+
+		await imported.main()
+	}
+	await run()
+}, import.meta)
---
+++ extension/test/tests/utils.bigint.ts
@@ -0,0 +1,20 @@
+import { bigintToRoundedPrettyDecimalString } from '../../app/ts/utils/bigint.js'
+import { describe, runIfRoot, should, run } from '../micro-should.js'
+import * as assert from 'assert'
+
+export async function main() {
+	describe('utils.bigint', () => {
+		should('display 1 ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(10n ** 18n, 18n, 4n), '1'))
+		should('display 0.2 ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(2n * 10n ** 17n, 18n, 4n), '0.2'))
+		should('display 00000001 ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(10n ** 10n, 18n, 4n), '0.00000001'))
+		should('display 100M ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(10n ** 26n, 18n, 4n), '100M'))
+		should('display 2.345k ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(234567n * 10n ** 16n, 18n, 4n), '2.345k'))
+		should('display -2.345k ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(-234567n * 10n ** 16n, 18n, 4n), '-2.345k'))
+		should('display -0.2345 ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(-234567n * 10n ** 12n, 18n, 4n), '-0.2345'))
+	})
+}
+
+await runIfRoot(async  () => {
+	await main()
+	await run()
+}, import.meta)
