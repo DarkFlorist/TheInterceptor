0a1cc35b073a9c0fbeb7d2d1bc201ef588388c08
Killari <Killari@gmail.com>
2023-01-16 18:10:31 +0200

    get rid of ts-node

--- extension/test/run-all.ts
+++ extension/test/run-all.ts
@@ -4,26 +4,37 @@ import * as path from 'path'
 import { fileURLToPath } from 'url'
 
 function *walkSync(dir: string) : Generator<string> {
-	const files = fs.readdirSync(dir, { withFileTypes: true });
+	const files = fs.readdirSync(dir, { withFileTypes: true })
 	for (const file of files) {
 		if (file.isDirectory()) {
-			yield* walkSync(path.join(dir, file.name));
+			yield* walkSync(path.join(dir, file.name))
 		} else {
-			yield path.join(dir, file.name);
+			yield path.join(dir, file.name)
 		}
 	}
 }
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url)
 const TEST_DIRECTORY = path.join(path.dirname(__filename), 'tests')
 
-await runIfRoot(async () => {
-	for (const file of walkSync(TEST_DIRECTORY)) {
-		const relativeFilePath = `./${ path.relative(path.dirname(__filename), file) }`
-		const imported = await import(relativeFilePath)
-		if( ! ('main' in imported) && typeof imported.main === 'function') throw new Error(`missing main function in ${ relativeFilePath }`)
+async function importValidateAndRun(relativeFilePath: string) {
+	const imported = await import(relativeFilePath)
+	if( ! ('main' in imported) && typeof imported.main === 'function') throw new Error(`missing main function in ${ relativeFilePath }`)
+	await imported.main()
+}
 
-		await imported.main()
+await runIfRoot(async () => {
+	const filesToRun = process.argv.slice(2)
+	if ( filesToRun.length > 0) { // run only specified ones
+		for (const relativeFilePath of filesToRun) {
+			await importValidateAndRun(relativeFilePath)
+		}
+	} else { // run all files
+		for (const file of walkSync(TEST_DIRECTORY)) {
+			if ( path.extname(file) !== '.js') continue
+			const relativeFilePath = `./${ path.relative(path.dirname(__filename), file) }`
+			await importValidateAndRun(relativeFilePath)
+		}
 	}
 	await run()
 }, import.meta)
