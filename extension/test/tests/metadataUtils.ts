11a362c69e8fbdfc130e093b9224fe783d435de5
Killari <Killari@gmail.com>
2023-03-30 10:57:19 +0300

    fix test

--- extension/test/tests/metadataUtils.ts
+++ extension/test/tests/metadataUtils.ts
@@ -5,7 +5,7 @@ import * as assert from 'assert'
 export async function main() {
 	describe('getAddressMetaData', () => {
 		should('contain USDC Coin', () => {
-			const metadata = getAddressMetaData(0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48n, undefined)
+			const metadata = getAddressMetaData(0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48n, { addressInfos: [], contacts: [] })
 			assert.equal(metadata.name, 'USD Coin')
 		})
 	})
