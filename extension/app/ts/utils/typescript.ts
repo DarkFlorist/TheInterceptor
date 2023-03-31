0dbf019f30a0c12777b6ee9264a1e392b56dda99
Killari <Killari@gmail.com>
2023-03-31 10:41:23 +0300

    Storage keys typing

--- extension/app/ts/utils/typescript.ts
+++ extension/app/ts/utils/typescript.ts
@@ -50,18 +50,39 @@ export function assertIsObject(maybe: unknown): asserts maybe is Object {
 	if (!isObject(maybe)) throw new Error(`Expected object but got ${ typeof maybe }`)
 }
 
-export async function browserStorageLocalGet(keys: string | string[]) {
-	return await browser.storage.local.get(keys) as Promise<Record<string, JSONEncodeable>>
+type StorageKey = 'activeSigningAddress'
+	| 'activeSimulationAddress'
+	| 'addressInfos'
+	| 'page'
+	| 'useSignersAddressAsActiveAddress'
+	| 'websiteAccess'
+	| 'activeChain'
+	| 'simulationMode'
+	| 'pendingAccessRequests'
+	| 'contacts'
+	| 'makeMeRich'
+	| 'addressbookTabId'
+	| 'ConfirmationWindowPromise'
+	| 'ChainChangeConfirmationPromise'
+	| 'PersonalSignPromise'
+	| 'InterceptorAccessRequestPromise'
+	| 'simulationResults'
+	| 'signerName'
+	| 'currentTabId'
+	| `tabState_${ number }`
+
+export async function browserStorageLocalGet(keys: StorageKey | StorageKey[]) {
+	return await browser.storage.local.get(keys) as Promise<Record<StorageKey, JSONEncodeable>>
 }
 
 type JSONEncodeable = string | number | boolean | { [x: string]: JSONEncodeable } | ReadonlyArray<JSONEncodeable>
 
-export async function browserStorageLocalSet(key: string, value: JSONEncodeable) {
+export async function browserStorageLocalSet(key: StorageKey, value: JSONEncodeable) {
 	return await browser.storage.local.set({ [key]: value })
 }
 
-export async function browserStorageLocalSingleGetWithDefault(key: string, valueIfMissing: unknown) {
-	const value = await browser.storage.local.get(key) as Record<string, unknown>
+export async function browserStorageLocalSingleGetWithDefault(key: StorageKey, valueIfMissing: unknown) {
+	const value = await browser.storage.local.get(key) as Record<StorageKey, unknown>
 	if (value[key] === undefined) return valueIfMissing
 	return value[key]
 }
