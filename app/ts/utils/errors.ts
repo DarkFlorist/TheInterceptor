9aa02cb566915a3b1b093232ea1a03233b90a137
KillariDev <killari@gmail.com>
2023-08-17 15:09:57 +0300

    add comment about the errors

--- app/ts/utils/errors.ts
+++ app/ts/utils/errors.ts
@@ -28,6 +28,7 @@ export class FetchResponseError extends ErrorWithData {
 }
 
 export function isFailedToFetchError(error: Error) {
+	// failed to fetch is thrown by Chrome if there's no connection to node and FireFox throws NetworkError instead
 	if (error.message.includes('Failed to fetch') || error.message.includes('NetworkError when attempting to fetch resource')) return true
 	return false
 }
