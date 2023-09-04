02bafa59042aaa7e627da04b7c21273766ca636b
KillariDev <killari@gmail.com>
2023-09-04 17:19:21 +0300

    set high fee to be 10x basefee or 10x estimated gas price

--- app/ts/simulation/protectors/feeOops.ts
+++ app/ts/simulation/protectors/feeOops.ts
@@ -2,10 +2,8 @@ import { Simulator } from '../simulator.js'
 import { EthereumUnsignedTransaction } from '../../types/wire-types.js'
 import { SimulationState } from '../../types/visualizer-types.js'
 
-const MAX_FEE = 2000n * 10n**9n
-
-export async function feeOops(transaction: EthereumUnsignedTransaction, _simulator: Simulator, _simulationState: SimulationState) {
-	if (transaction.type === '1559') { if (transaction.maxPriorityFeePerGas < MAX_FEE) return }
-	else if (transaction.gasPrice < MAX_FEE) return
+export async function feeOops(transaction: EthereumUnsignedTransaction, simulator: Simulator, _simulationState: SimulationState) {
+	if (transaction.type === '1559') { if (transaction.maxPriorityFeePerGas < 10n**9n) return } // 1.0 nanoEth/gas 
+	else if (transaction.gasPrice < await simulator.ethereum.getGasPrice() * 10n) return // 10 times the estimate gas pice
 	return 'BIG_FEE'
 }
