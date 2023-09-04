aad2f8977597a37250aec771c301bde416fd7247
KillariDev <killari@gmail.com>
2023-09-04 17:20:11 +0300

    spaces

--- app/ts/simulation/protectors/feeOops.ts
+++ app/ts/simulation/protectors/feeOops.ts
@@ -3,7 +3,7 @@ import { EthereumUnsignedTransaction } from '../../types/wire-types.js'
 import { SimulationState } from '../../types/visualizer-types.js'
 
 export async function feeOops(transaction: EthereumUnsignedTransaction, simulator: Simulator, _simulationState: SimulationState) {
-	if (transaction.type === '1559') { if (transaction.maxPriorityFeePerGas < 10n**9n) return } // 1.0 nanoEth/gas 
+	if (transaction.type === '1559') { if (transaction.maxPriorityFeePerGas < 10n ** 9n) return } // 1.0 nanoEth/gas 
 	else if (transaction.gasPrice < await simulator.ethereum.getGasPrice() * 10n) return // 10 times the estimate gas pice
 	return 'BIG_FEE'
 }
