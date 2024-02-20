204db61e1f61bf7a4bfca7b2bc63647b72968d5c
KillariDev <killari@gmail.com>
2024-02-20 13:26:12 +0200

    set https://2.horswap.eth.limo/ as alternative for uniswap

--- app/ts/utils/websiteData.ts
+++ app/ts/utils/websiteData.ts
@@ -30,7 +30,7 @@ const websiteMetaData: WebsiteMetaData = {
 	'app.uniswap.org': {
 		name: 'Uniswap',
 		message: `Uniswap V4 encounters functionality issues when using The Interceptor in Simulation Mode due to its reliance on centralized components. Additionally, the Uniswap V4 interface imposes an extra fee on its users. It is advisable to use alternative interface that does not have these limitations.`,
-		suggestedAlternative: 'https://bafybeib2jsrxvqwm4hscnwtp5pcd2gpxdaltk745hffsnktb2sa3humm4i.ipfs.dweb.link',
+		suggestedAlternative: 'https://2.horswap.eth.limo/',
 		externalRpc: true,
 	},
 	'octant.app': {
