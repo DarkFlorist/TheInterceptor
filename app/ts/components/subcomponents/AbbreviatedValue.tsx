063c34f77f769c5f11fd9ebc81549ff4c70731ed
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2024-04-09 15:00:45 +0800

    fix type error for string split

--- app/ts/components/subcomponents/AbbreviatedValue.tsx
+++ app/ts/components/subcomponents/AbbreviatedValue.tsx
@@ -5,28 +5,29 @@ export const AbbreviatedValue = ({ floatValue }: { floatValue: number }) => {
 		{ value: 1e3, symbol: 'k' },
 	];
 
-	// Check if the value is negative
-	const isNegative = floatValue < 0;
-	// Convert the value to positive for abbreviation logic
-	const valueForAbbreviation = isNegative ? -floatValue : floatValue;
+	const sign = floatValue < 0 ? '-' : '';
+	const absoluteValue = Math.abs(floatValue)
 
+	// display prefixed values
 	for (const prefix of prefixes) {
-		if (valueForAbbreviation >= prefix.value) {
-			// Apply the abbreviation and add a negative sign if the original value was negative
-			return <>{isNegative ? '-' : ''}{toFixedLengthDigits(valueForAbbreviation / prefix.value) + prefix.symbol}</>
+		if (absoluteValue >= prefix.value) {
+			return <>{sign}{toFixedLengthDigits(absoluteValue / prefix.value) + prefix.symbol}</>
 		}
 	}
 
-	// if value is a fraction of 1
-	if (valueForAbbreviation && valueForAbbreviation % 1 === valueForAbbreviation) {
-		const [coefficient, exponent] = valueForAbbreviation.toExponential().split('e')
+	// display for values that are a fraction of 1
+	if (absoluteValue && absoluteValue % 1 === absoluteValue) {
+		const [coefficient, exponent] = absoluteValue.toExponential().split('e')
+
+		// coefficient and exponent should always return string for absolute values but String.split thinks otherwise
+		if (!coefficient || !exponent) return floatValue
+
 		const leadingZerosCount = Math.abs(Number.parseInt(exponent)) - 1
 		const significantDigits = coefficient.replace('.', '')
-		return <>{isNegative ? '-' : ''}0.<small>{'0'.repeat(leadingZerosCount)}</small>{significantDigits}</>
+		return <>{sign}0.<small>{'0'.repeat(leadingZerosCount)}</small>{significantDigits}</>
 	}
 
-	// Return the original value with a negative sign if it was negative
-	return <>{isNegative ? '-' : ''}{toFixedLengthDigits(valueForAbbreviation)}</>
+	return <>{sign}{toFixedLengthDigits(absoluteValue)}</>
 }
 
 function toFixedLengthDigits(num: number, max = 5) {
