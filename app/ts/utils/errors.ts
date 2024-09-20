b39f12cffae58ddaf9a25ec69db50da32936f366
KillariDev <killari@gmail.com>
2024-09-20 17:49:27 +0300

    remove space

--- app/ts/utils/errors.ts
+++ app/ts/utils/errors.ts
@@ -28,7 +28,7 @@ export const isNewBlockAbort = (error: Error) => error.message.includes(NEW_BLOC
 
 export function printError(error: unknown) {
 	if (error instanceof Error && 'data' in error) {
-		return console.error(`Error: ${ error.message }\n${ JSON.stringify(error.data) }\n ${ error.stack !== undefined ? error.stack : ''}`)
+		return console.error(`Error: ${ error.message }\n${ JSON.stringify(error.data) }\n${ error.stack !== undefined ? error.stack : ''}`)
 	}
 	return console.error(error)
 }
