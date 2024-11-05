769a686cd6349b7b90ab5d87234695cbbeb5da46
KillariDev <13102010+KillariDev@users.noreply.github.com>
2024-11-05 14:08:14 +0200

    Update app/ts/utils/errors.ts
    Co-authored-by: Jubal Mabaquiao <jubal.mabaquiao@gmail.com>

--- app/ts/utils/errors.ts
+++ app/ts/utils/errors.ts
@@ -30,7 +30,7 @@ export function printError(error: unknown) {
 	if (error instanceof Error) {
 		try {
 			if ('data' in error) return console.error(`Error: ${ error.message }\n${ JSON.stringify(error.data) }\n${ error.stack !== undefined ? error.stack : ''}`)
-			return console.error(`Error: ${ error.message }\n${ error.stack !== undefined ? error.stack : ''}`)
+			return console.error(`Error: ${ error.message }\n${ error.stack || ''}`)
 		} catch(stringifyError) {
 			console.error(stringifyError)
 		}
