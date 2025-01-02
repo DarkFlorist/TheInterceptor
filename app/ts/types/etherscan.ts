0a6482d3f92c369c03bcfcff34819fcb06644f44
KillariDev <killari@gmail.com>
2025-01-02 10:04:06 +0200

    add sourcify to abi retrival

--- app/ts/types/etherscan.ts
+++ app/ts/types/etherscan.ts
@@ -13,11 +13,22 @@ export const EtherscanSourceCodeResult = funtypes.Object({
 	}))
 }).asReadonly()
 
-
 // Not full result definition, only entries that we consume
 // https://docs.etherscan.io/api-endpoints/contracts#get-contract-abi-for-verified-contract-source-codes
 export type EtherscanGetABIResult = funtypes.Static<typeof EtherscanGetABIResult>
 export const EtherscanGetABIResult = funtypes.Object({
 	status: funtypes.Union(funtypes.Literal('1').withParser(LiteralConverterParserFactory('1', 'success' as const)), funtypes.Literal('0').withParser(LiteralConverterParserFactory('0', 'failure' as const))),
 	result: funtypes.String
+}).asReadonly()
+
+export type SourcifyMetadataResult = funtypes.Static<typeof SourcifyMetadataResult>
+export const SourcifyMetadataResult = funtypes.Object({
+	compiler: funtypes.Unknown,
+	language: funtypes.Unknown,
+	output: funtypes.Object({
+		abi: funtypes.Array(funtypes.Unknown)
+	}),
+	settings: funtypes.Unknown,
+	sources: funtypes.Unknown,
+	version: funtypes.Unknown,
 }).asReadonly()
