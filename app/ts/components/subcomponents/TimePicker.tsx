a5a28aebaeed17cd936798332f2fd8eeb9a352c5
KillariDev <killari@gmail.com>
2025-04-02 16:26:03 +0300

    separate TimePickerModeViews

--- app/ts/components/subcomponents/TimePicker.tsx
+++ app/ts/components/subcomponents/TimePicker.tsx
@@ -1,6 +1,7 @@
 import { Signal, useSignal } from '@preact/signals'
 import { DropDownMenu } from './DropDownMenu.js'
 import { JSX } from 'preact/jsx-runtime'
+import { assertNever } from '../../utils/typescript.js'
 
 const timePickerModeDownOptions = ['Until', 'For', 'No Delay'] as const
 const timePickerModeDownOptionsWithoutNoDelay = ['Until', 'For'] as const
@@ -8,6 +9,32 @@ const timePickerDeltaOptions = ['Seconds', 'Minutes', 'Hours', 'Days', 'Weeks', 
 
 export type DeltaUnit = typeof timePickerDeltaOptions[number]
 export type TimePickerMode = typeof timePickerModeDownOptions[number]
+
+type TimePickerModeViewsParams = {
+	mode: Signal<TimePickerMode>
+	absoluteTime: Signal<string>
+	deltaValue: Signal<number>
+	deltaUnit: Signal<DeltaUnit>
+
+	timePickerDeltaOptionsSignal: Signal<readonly DeltaUnit[]>
+
+	changeDeltaUnit: (newOption: DeltaUnit) => void
+	absoluteTimeChanged: (event: JSX.TargetedInputEvent<HTMLInputElement>) => void
+	changeDeltaValue: (event: JSX.TargetedInputEvent<HTMLInputElement>) => void
+}
+
+const TimePickerModeViews = ({ mode, absoluteTime, timePickerDeltaOptionsSignal, deltaValue, deltaUnit, changeDeltaUnit, absoluteTimeChanged, changeDeltaValue }: TimePickerModeViewsParams) => {
+	switch(mode.value) {
+		case 'No Delay': return <></>
+		case 'Until': return <input type = 'datetime-local' class = 'datetime' value = { absoluteTime.value } onInput = { absoluteTimeChanged } />
+		case 'For': return <div>
+			<input class = 'input' style = 'width: 50px; margin-right: 10px; vertical-align: unset; text-align: center;' type = 'number' value = { deltaValue.value } onInput = { changeDeltaValue } />
+			<DropDownMenu selected = { deltaUnit } dropDownOptions = { timePickerDeltaOptionsSignal } onChangedCallBack = { changeDeltaUnit } buttonClassses = { 'btn btn--outline is-small' }/>
+		</div>
+		default: assertNever(mode.value)
+	}
+}
+
 type TimePickerParams = {
 	mode: Signal<TimePickerMode>
 	absoluteTime: Signal<string>
@@ -44,16 +71,7 @@ export const TimePicker = ({ mode, absoluteTime, deltaValue, deltaUnit, onChange
 			<p class = 'paragraph' style = 'align-content: center;'> { startText } </p>
 			<div style = 'display: grid; grid-template-columns: auto auto; column-gap: 10px; padding-left: 5px'>
 				<DropDownMenu selected = { mode } dropDownOptions = { timePickerModeDownOptionsSignal } onChangedCallBack = { changeMode } buttonClassses = { 'btn btn--outline is-small' }/>
-				{ mode.value === 'No Delay' ? <></> : <>
-					{ mode.value === 'Until' ? (
-						<input type = 'datetime-local' class = 'datetime' value = { absoluteTime.value } onInput = { absoluteTimeChanged } />
-					) : (
-						<div>
-							<input class = 'input' style = 'width: 50px; margin-right: 10px; vertical-align: unset; text-align: center;' type = 'number' value = { deltaValue.value } onInput = { changeDeltaValue } />
-							<DropDownMenu selected = { deltaUnit } dropDownOptions = { timePickerDeltaOptionsSignal } onChangedCallBack = { changeDeltaUnit } buttonClassses = { 'btn btn--outline is-small' }/>
-						</div>
-					)}
-				</> }
+				<TimePickerModeViews mode = { mode } absoluteTime = { absoluteTime } deltaValue = { deltaValue } deltaUnit = { deltaUnit } timePickerDeltaOptionsSignal = { timePickerDeltaOptionsSignal } changeDeltaUnit = { changeDeltaUnit } absoluteTimeChanged = { absoluteTimeChanged } changeDeltaValue = { changeDeltaValue }/>
 			</div>
 		</div>
 	</div>
