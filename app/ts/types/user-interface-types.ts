92813ef44bb95e85f370cf93cc7f5f65b6e764d4
KillariDev <killari@gmail.com>
2025-12-04 16:04:00 +0200

    remove extra commas

--- app/ts/types/user-interface-types.ts
+++ app/ts/types/user-interface-types.ts
@@ -31,27 +31,27 @@ export type AddAddressParam = {
 }
 
 export type HomeParams = {
-	changeActiveAddress: () => void,
-	makeMeRich: Signal<boolean>,
-	keepSelectedAddressRichEvenIfIChangeAddress: Signal<boolean>,
-	activeAddresses: Signal<AddressBookEntries>,
-	tabState: TabState | undefined,
-	activeSimulationAddress: bigint | undefined,
-	activeSigningAddress: bigint | undefined,
-	useSignersAddressAsActiveAddress: boolean,
-	simVisResults: SimulationAndVisualisationResults | undefined,
-	rpcNetwork: Signal<RpcNetwork | undefined>,
-	setActiveRpcAndInformAboutIt: (entry: RpcEntry) => void,
-	simulationMode: Signal<boolean>,
-	tabIconDetails: TabIconDetails,
-	currentBlockNumber: bigint | undefined,
-	renameAddressCallBack: RenameAddressCallBack,
-	editEnsNamedHashCallBack: EditEnsNamedHashCallBack,
-	rpcConnectionStatus: Signal<RpcConnectionStatus>,
+	changeActiveAddress: () => void
+	makeMeRich: Signal<boolean>
+	keepSelectedAddressRichEvenIfIChangeAddress: Signal<boolean>
+	activeAddresses: Signal<AddressBookEntries>
+	tabState: TabState | undefined
+	activeSimulationAddress: bigint | undefined
+	activeSigningAddress: bigint | undefined
+	useSignersAddressAsActiveAddress: boolean
+	simVisResults: SimulationAndVisualisationResults | undefined
+	rpcNetwork: Signal<RpcNetwork | undefined>
+	setActiveRpcAndInformAboutIt: (entry: RpcEntry) => void
+	simulationMode: Signal<boolean>
+	tabIconDetails: TabIconDetails
+	currentBlockNumber: bigint | undefined
+	renameAddressCallBack: RenameAddressCallBack
+	editEnsNamedHashCallBack: EditEnsNamedHashCallBack
+	rpcConnectionStatus: Signal<RpcConnectionStatus>
 	rpcEntries: Signal<RpcEntries>
-	simulationUpdatingState: SimulationUpdatingState | undefined,
-	simulationResultState: SimulationResultState | undefined,
-	interceptorDisabled: boolean,
+	simulationUpdatingState: SimulationUpdatingState | undefined
+	simulationResultState: SimulationResultState | undefined
+	interceptorDisabled: boolean
 	preSimulationBlockTimeManipulation: Signal<BlockTimeManipulation | undefined>
 	makeMeRichList: Signal<readonly AddressBookEntry[]>
 }
