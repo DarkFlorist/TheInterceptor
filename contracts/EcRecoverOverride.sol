552c7a45f1a2cf902b70d931e9e5c40faaa8de35
KillariDev <13102010+KillariDev@users.noreply.github.com>
2026-07-22 11:59:27 +0000

    Use tab indentation in Solidity sources

--- contracts/EcRecoverOverride.sol
+++ contracts/EcRecoverOverride.sol
@@ -2,16 +2,16 @@
 pragma solidity 0.8.18;
 
 contract EcRecoverOverride {
-    mapping(bytes32 => address) private overrideToAddress;
+	mapping(bytes32 => address) private overrideToAddress;
 
-    fallback(bytes calldata input) external returns (bytes memory) {
-        (bytes32 hash, uint8 v, bytes32 r, bytes32 s) = abi.decode(input, (bytes32, uint8, bytes32, bytes32));
-        address overriddenAddress = overrideToAddress[keccak256(abi.encode(hash, v, r, s))];
-        if (overriddenAddress == address(0)) {
-            (bool success, bytes memory data) = address(0x0000000000000000000000000000000000123456).call{gas: 10000}(input);
-            require(success, 'failed to call moved ecrecover at address 0x0000000000000000000000000000000000123456');
-            return data;
-        }
-        return abi.encode(overriddenAddress);
-    }
+	fallback(bytes calldata input) external returns (bytes memory) {
+		(bytes32 hash, uint8 v, bytes32 r, bytes32 s) = abi.decode(input, (bytes32, uint8, bytes32, bytes32));
+		address overriddenAddress = overrideToAddress[keccak256(abi.encode(hash, v, r, s))];
+		if (overriddenAddress == address(0)) {
+			(bool success, bytes memory data) = address(0x0000000000000000000000000000000000123456).call{gas: 10000}(input);
+			require(success, 'failed to call moved ecrecover at address 0x0000000000000000000000000000000000123456');
+			return data;
+		}
+		return abi.encode(overriddenAddress);
+	}
 }
--- contracts/GetCode.sol
+++ contracts/GetCode.sol
@@ -2,13 +2,13 @@
 pragma solidity 0.8.18;
 
 library GetCode {
-    function at(address target) public view returns (bytes memory code) {
-        assembly {
-            let size := extcodesize(target)
-            code := mload(0x40)
-            mstore(0x40, add(code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
-            mstore(code, size)
-            extcodecopy(target, add(code, 0x20), 0, size)
-        }
-    }
+	function at(address target) public view returns (bytes memory code) {
+		assembly {
+			let size := extcodesize(target)
+			code := mload(0x40)
+			mstore(0x40, add(code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+			mstore(code, size)
+			extcodecopy(target, add(code, 0x20), 0, size)
+		}
+	}
 }
