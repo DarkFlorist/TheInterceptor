523b40717751aaebd277549fa1c7b203dd78e539
KillariDev <13102010+KillariDev@users.noreply.github.com>
2026-08-02 20:23:08 +0300

    Fix validation and UI boundary bugs (#1571)
    * Fix validation and UI boundary bugs
    
    * Fix multidimensional EIP-712 visualization

--- app/ts/utils/websiteData.ts
+++ app/ts/utils/websiteData.ts
@@ -37,7 +37,7 @@ const websiteMetaData: WebsiteMetaData = {
 		name: 'Octant',
 		externalRpc: true,
 	},
-	'app.aave.com/': {
+	'app.aave.com': {
 		name: 'Aave',
 		externalRpc: true,
 	},
--- test/tests/utils.bigint.test.ts
+++ test/tests/utils.bigint.test.ts
@@ -1,4 +1,4 @@
-import { bigintToRoundedPrettyDecimalString } from '../../app/ts/utils/bigint.js'
+import { bigintToRoundedPrettyDecimalString, calculateWeightedPercentile } from '../../app/ts/utils/bigint.js'
 import { describe, test } from 'bun:test'
 import * as assert from 'assert'
 
@@ -13,4 +13,15 @@ describe('utils.bigint', () => {
 	test('display -2345.67 ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(-234567n * 10n ** 16n, 18n, 4), '-2345.67'))
 	test('display -0.2346 ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(-234567n * 10n ** 12n, 18n, 4), '-0.2346'))
 	test('display -10k ETH', () => assert.equal(bigintToRoundedPrettyDecimalString(-(10n ** 22n), 18n, 4), '-10k'))
+	test('calculates the highest weighted percentile without indexing past the data', () => {
+		assert.equal(calculateWeightedPercentile([{ dataPoint: 10n, weight: 1n }], 100n), 10n)
+		assert.equal(calculateWeightedPercentile([
+			{ dataPoint: 10n, weight: 1n },
+			{ dataPoint: 20n, weight: 3n },
+		], 25n), 10n)
+		assert.equal(calculateWeightedPercentile([
+			{ dataPoint: 10n, weight: 1n },
+			{ dataPoint: 20n, weight: 3n },
+		], 26n), 20n)
+	})
 })
